c6e7795f74a56e1b1f3f1dc763715082c807bbcf
Eric Pritchett <eric.a.pritchett.che@gmail.com>
2026-06-05 22:40:14 -0500

    chore: add html treesitter

--- init.lua
+++ init.lua
@@ -540,7 +540,7 @@ success, result = pcall(require, "tree-sitter-manager")
 if success then
   result.setup({
     -- Default Options
-    ensure_installed = { "python", "typescript", "sql", "scss", "css" }, -- list of parsers to install at the start of a neovim session. If set to "all", install all parsers.
+    ensure_installed = { "python", "typescript", "sql", "scss", "css", "html" }, -- list of parsers to install at the start of a neovim session. If set to "all", install all parsers.
     -- border = nil, -- border style for the window (e.g. "rounded", "single"), if nil, use the default border style defined by 'vim.o.winborder'. See :h 'winborder' for more info.
     -- auto_install = false, -- if enabled, install missing parsers when editing a new file
     highlight = true, -- treesitter highlighting is enabled by default

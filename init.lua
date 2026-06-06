ce3555072ebeeaf709971c6c453824c054c21ad3
Eric Pritchett <eric.a.pritchett.che@gmail.com>
2026-06-05 21:18:07 -0500

    chore: add tree sitter manager

--- init.lua
+++ init.lua
@@ -532,3 +532,20 @@ if success then
 else
   vim.notify("Failed to setup neo-tree", vim.log.levels.ERROR)
 end
+
+vim.pack.add({
+  { src = "https://github.com/romus204/tree-sitter-manager.nvim" },
+})
+success, result = pcall(require, "tree-sitter-manager")
+if success then
+  result.setup({
+    -- Default Options
+    ensure_installed = { "python", "typescript", "sql", "scss", "css" }, -- list of parsers to install at the start of a neovim session. If set to "all", install all parsers.
+    -- border = nil, -- border style for the window (e.g. "rounded", "single"), if nil, use the default border style defined by 'vim.o.winborder'. See :h 'winborder' for more info.
+    -- auto_install = false, -- if enabled, install missing parsers when editing a new file
+    highlight = true, -- treesitter highlighting is enabled by default
+    -- languages = {}, -- override or add new parser sources
+  })
+else
+  vim.notify("Failed to setup tree-sitter-manager", vim.log.levels.ERROR)
+end
